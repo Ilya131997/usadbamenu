2c16e07dc36275694732f9b502f5f3532a4ce73f
Ilya131997 <ilya.yakim131997@gmail.com>
2023-03-20 23:36:39 +0300

    3 commit, adds: templates, model, urls, views

--- usadba/menu/urls.py
+++ usadba/menu/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('product/', Product.as_view(), name='product')
 ]
--- usadba/menu/views.py
+++ usadba/menu/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from menu.models import *
+
 
 def index(request):
-    return HttpResponse('KYKY')
+    return render(request, 'menu/homemenu.html')
+
+class Product(ListView):
+    model = Product
+    template_name = 'menu/product.html'

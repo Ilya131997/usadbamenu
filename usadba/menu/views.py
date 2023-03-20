e1a3ae94cdcf5d4338a1b6b23ccfa5cbf9ed7305
Ilya131997 <ilya.yakim131997@gmail.com>
2023-03-21 00:21:01 +0300

    4 commit, products

--- usadba/menu/views.py
+++ usadba/menu/views.py
@@ -2,12 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from menu.models import *
+from .models import *
 
 
 def index(request):
     return render(request, 'menu/homemenu.html')
 
+
 class Product(ListView):
     model = Product
     template_name = 'menu/product.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Продукты'
+        return context
